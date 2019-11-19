c5873c98631f8457a30d98e49c32faebf8970bfa
danisnowman <danisnowman@gmail.com>
2019-11-19 17:43:33 -0600

    added magic redis func
    func does JSON->string->pub->sub->string->JSON, but blocking

--- gui/server.js
+++ gui/server.js
@@ -1,7 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const redis = require('redis');
+const publisher = redis.createClient();
+const app = express();
+const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello From Express'))
+app.get('/', (req, res) => res.send('Hello From Express'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+var j={
+    "name":"Andrea"
+};
+
+function JSONpub(json) {
+    console.log("Sending JSON")
+    json_string = JSON.stringify(json)
+    publisher.publish("test",json_string);
+}
+
+JSONpub(j)
--- lex/lex.py
+++ lex/lex.py
@@ -1,11 +1,46 @@
 from flask import Flask
+import redis, json
 
 app = Flask(__name__)
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+p = r.pubsub()
+p.subscribe("test")
 
 
 @app.route("/")
 def index():
     return "Hello from Flask!"
 
+def subJSON():
+    """Reads message to JSON.
+
+    Reads constantly if there's any new message 
+    in the channel, if so it parses it into
+    a JSON format and returns it.
+    """
+    json_message = None
+    print("Running subJSON()")
+    for message in p.listen():
+        print(f"message.get(data):{message.get('data')}") # DELETE LATER
+        message_data = message.get('data')
+
+        if not isinstance(message_data, int):
+            print("\nIF PASSED")
+            json_string = message.get('data') # DELETE LATER
+            print(f"json_string:{json_string}")
+            
+            json_message = json.loads(json_string)
+            print(f"json.get(name):{json_message.get('name')}")
+        print(f"message: {message}") # DELETE LATER 
+    return json_message
+
+new_json = subJSON()
+
+# print(f"new_json.get('name'):{new_json.get('name')}")
+print(type(new_json))
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
+
+
+
