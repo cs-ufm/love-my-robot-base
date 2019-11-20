2acd86017670e8bb834d358a4b0c98be5bd28b26
Katherine Mazariegos <katherinemazariegos@ufm.edu>
2019-11-20 08:25:20 -0600

    Animals functions added

--- lex/lex.py
+++ lex/lex.py
@@ -1,6 +1,7 @@
 import asyncio
 import sys
 import cozmo
+import time
 from flask import Flask
 
 from cozmo.lights import blue_light, Color, green_light, Light, red_light, white_light, off_light
@@ -9,8 +10,10 @@ from cozmo.util import degrees, distance_mm, radians, speed_mmps
 app = Flask(__name__)
 
 #Cozmo functions
-def sayhello(robot: cozmo.robot.Robot):
-    robot.say_text("Hello World").wait_for_completed()
+async def sayhello(robot: cozmo.robot.Robot):
+    async with robot.perform_off_charger():
+        action = robot.say_text("Hello World")
+        await action.wait_for_completed()
 
 def move(robot: cozmo.robot.Robot):
     # Drive forwards for 150 millimeters at 50 millimeters-per-second.
@@ -25,6 +28,27 @@ def turn(robot: cozmo.robot.Robot):
     # Note: To turn to the right, just use a negative number.
     robot.turn_in_place(degrees(90)).wait_for_completed()
 
+def lift(robot: cozmo.robot.Robot):
+    # Tell the head motor to start lowering the head (at 5 radians per second)
+    #robot.move_head(-5)
+    # Tell the lift motor to start lowering the lift (at 5 radians per second)
+    robot.move_lift(-5)
+    # Tell Cozmo to drive the left wheel at 25 mmps (millimeters per second),
+    # and the right wheel at 50 mmps (so Cozmo will drive Forwards while also
+    # turning to the left
+    #robot.drive_wheels(25, 50)
+
+#Animations
+def celebration(robot: cozmo.robot.Robot):
+    robot.play_anim_trigger(cozmo.anim.Triggers.CodeLabWin).wait_for_completed()  
+
+#Animals
+def duck(robot: cozmo.robot.Robot):
+    robot.play_anim_trigger(cozmo.anim.Triggers.CodeLabDuck).wait_for_completed()    
+
+def frog(robot: cozmo.robot.Robot):
+    robot.play_anim_trigger(cozmo.anim.Triggers.CodeLabDuck).wait_for_completed()  
+
 
 
 @app.route("/e")
@@ -33,7 +57,7 @@ def index():
 
 @app.route('/')
 def hello_world():
-    cozmo.run_program(moveback)
+    cozmo.run_program(happy)
 
     return 'Hello, World!'
 
