f246a214b148bad3871a01a35939cd4460f27089
danisnowman <danisnowman@gmail.com>
2019-11-20 12:42:19 -0600

    added multiple parameters option

--- test1.py
+++ test1.py
@@ -2,5 +2,5 @@ import cozmo
 from cozmo.lights import blue_light, Color, green_light, Light, red_light, white_light, off_light
 from cozmo.util import degrees, distance_mm, radians, speed_mmps
 def cozmo_program(robot: cozmo.robot.Robot):
-    robot.say_text('NAAAAAIS').wait_for_completed()
+    robot.say_text('NICE').wait_for_completed()
 cozmo.run_program(cozmo_program)
