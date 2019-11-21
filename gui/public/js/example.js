c3a345b20946e87fb3422bc3438c7cf45c8a98ea
andresryes <enriquebolanos@ufm.edu>
2019-11-20 23:16:50 -0600

    added gui files

---
+++ gui/public/js/example.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('.li1').click(function() {
+        console.log("hola")
+        let name = $(this).text()
+        console.log(name);
+        if (name && name.length > 0) {
+            $.post('/delete-user', {name:name}, function(data, status) {
+                console.log(`${data.message} and status is ${status}`)
+                alert(data.message)
+                setTimeout(function() {
+                    location.reload();
+                }, 2000);
+            })
+        }
+    })
+    $('.send').click(function() {
+        let newName = $('#nombre').val()
+        console.log(newName);
+        if (newName && newName.length > 0) {
+            $.post('/save-user', {name:newName}, function(data, status) {
+                console.log(`${data.message} and status is ${status}`)
+                alert(data.message)
+                setTimeout(function() {
+                    location.reload();
+                }, 2000);
+            })
+        }
+    })
+})
--- gui/server.js
+++ gui/server.js
@@ -1,7 +1,38 @@
 const express = require('express')
+const pug = require('pug');
+var bodyParser = require('body-parser');
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello From Express'))
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let names = ['Jose', 'Luis']
+
+app.get('/', (req, res) => res.sendFile('index.html'))
+app.get('/pug', (req, res) => res.render('pugIndex', {names}))
+app.post('/save-user', function(req, res) {
+    console.log(req.body);
+    names.push(req.body.name);
+    res.json({message:"New name added"})
+})
+app.post('/delete-user', function(req, res) {
+    console.log(req.body);
+    let found = 0
+    for( var i = 0; i < names.length; i++){ 
+        if ( names[i] === req.body.name) {
+          names.splice(i, 1); 
+          found = 1
+        }
+     }
+    // names.delete(req.body.name);
+    if(found == 1){
+        res.json({message:"User deleted"})
+    }
+    res.json({message:"User not found"})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
