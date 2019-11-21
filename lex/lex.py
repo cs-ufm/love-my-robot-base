50cbc632ba5ed93b234dc29d3222d320908cb416
abnerxch <axocop@ufm.edu>
2019-11-21 10:39:57 -0600

    Roll a cube code

--- lex/lex.py
+++ lex/lex.py
@@ -254,13 +254,25 @@ async def pop_a_wheelie():
     '''
     return f'print("Cozmo is waiting until he sees a cube")\ncube = await robot.world.wait_for_observed_light_cube()\nprint("Cozmo found a cube, and will now attempt to pop a wheelie on it")\naction = robot.pop_a_wheelie(cube, num_retries=2)\nawait action.wait_for_completed()\n'
 
-def roll_a_cube():
+async def roll_a_cube():
     '''
     Tell Cozmo to roll a cube that is placed in front of him.
     This example demonstrates Cozmo driving to and rolling a cube.
     You must place a cube in front of Cozmo so that he can see it.
     The cube should be centered in front of him.
     '''
+
+    await robot.set_head_angle(degrees(-5.0)).wait_for_completed()
+
+    print("Cozmo is waiting until he sees a cube")
+    cube = await robot.world.wait_for_observed_light_cube()
+
+    print("Cozmo found a cube, and will now attempt to roll with it:")
+    # Cozmo will approach the cube he has seen and roll it
+    # check_for_object_on_top=True enforces that Cozmo will not roll cubes with anything on top
+    action = robot.roll_cube(cube, check_for_object_on_top=True, num_retries=2)
+    await action.wait_for_completed()
+    print("result:", action.result)
 
 if __name__ == "__main__":
     """We start asyncSUB() and Flask.
