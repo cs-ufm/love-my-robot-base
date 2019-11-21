4898c44d26cc859304e8a494c72f44c308037393
Maite de la Roca <delaroca.estrada21@gmail.com>
2019-11-21 15:33:01 -0600

    display and hide blocks

--- gui/public/js/actions.js
+++ gui/public/js/actions.js
@@ -1,4 +1,15 @@
-console.log("Hello World")
+let actions = ["move", "sounds", "cozmo-lights", "animations"]
 $( document ).ready(function() {
-    console.log( "ready!" );
+    for (let i = 0; i < actions.length; i++) {
+        $(`.${actions[i]}`).click(function(){
+            console.log("clicked")
+            document.getElementsByClassName(actions[i])[1].classList.remove('hide')
+
+            for (let j = 0; j < actions.length; j++){
+                if(j != i){
+                    document.getElementsByClassName(actions[j])[1].classList.add('hide')
+                }
+            }
+        });
+    }
 });
