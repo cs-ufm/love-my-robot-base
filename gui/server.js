5283ecc1ebbe22933920a5a90fd13631fcbd4af4
Fernando González <rmfernando@outlook.com>
2019-11-22 11:48:46 -0600

    actions route created.

--- gui/server.js
+++ gui/server.js
@@ -42,7 +42,7 @@ app.get('/', function(req, res){
 })
 
 app.get('/home', function (req, res) {
-    
+
     // include home.html
     const template = fs.readFileSync('views/home.html', 'utf8');
 
@@ -50,11 +50,7 @@ app.get('/home', function (req, res) {
     const keys_string = fs.readFileSync('./keys.json', 'utf8');
     const keys = JSON.parse(keys_string);
 
-    for (let key in keys) {
-        keys[key].gap = ((parseInt(key) + 1) * 140) - 20
-    }
-
-
+   
     //include navbar.html
     const nav = fs.readFileSync('views/navbar.html', 'utf8');
 
@@ -71,6 +67,45 @@ app.get('/home', function (req, res) {
     res.status(200).send(renderIndex)
 })
 
+
+app.get('/actions', function (req, res) {
+    const template = fs.readFileSync('views/index.html', 'utf8');
+
+    // get keys for todo
+    const keys_string = fs.readFileSync('./keys.json', 'utf8');
+    const keys = JSON.parse(keys_string);
+
+    for (let key in keys) {
+        keys[key].gap = ((parseInt(key) + 1) * 140) - 20
+    }
+
+    //include menu.html
+    const menu = fs.readFileSync('views/menu.html', 'utf8');
+
+    //include navbar.html
+    const nav = fs.readFileSync('views/navbar.html', 'utf8');
+
+    //include stack.html
+    const stack = fs.readFileSync('views/stack.html', 'utf8');
+
+    //include jumbotron.html
+    const jumbo = fs.readFileSync('views/jumbotron.html', 'utf8');
+
+    //include modal.html
+    const modal = fs.readFileSync('views/modal.html', 'utf8');
+
+    const renderIndex = Mustache.render(template, {
+        menuOptions: keys
+    }, {
+        menu,
+        nav,
+        jumbo,
+        stack,
+        modal
+    });
+
+    res.status(200).send(renderIndex)
+})
 
 
 //app.get('/', (req, res) => res.send('Hello From Express'))
