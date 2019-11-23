96a5729d291653f1766c0a1686eddb543ab0bbd8
Diego Quan <diegoquan101@gmail.com>
2019-11-23 12:44:26 -0600

    fixes fixes

--- lex/lex.py
+++ lex/lex.py
@@ -1,16 +1,13 @@
 
 from flask import Flask,render_template, request
 from flask_cors import CORS
-import json, sys
-
+import json, sys, os
+from datetime import datetime
 
 app = Flask(__name__)
-CORS(app)
-from datetime import datetime
-import os
-sys.path.append('c:/Users/justf/+/love-my-robot-base/lex/transpiled')
-#robot = cozmo.robot.Robot
-#measures = cozmo.util
+
+sys.path.append('c:/Users/justf/LMR/love-my-robot-base/lex/transpiled')
+
 big_string = ''
 def generate_code(test, cond):
     timestamp = datetime.now().minute
@@ -31,11 +28,11 @@ def generate_code(test, cond):
                 big_string = big_string +'   '+ data[y]
             f.write('\ndef run(cozmo_program):\n    cozmo.run_program(cozmo_program)')
             big_string = big_string + '\ndef run(cozmo_program):\n    cozmo.run_program(cozmo_program)'   
-    #import cozmo_generated_program as p
-    #try:
-        #p.run(p.cozmo_program)
-    #except:
-    #    print('DIDNT FINISH')
+    import cozmo_generated_program as p
+    try:
+        p.run(p.cozmo_program)
+    except:
+        print('DIDNT FINISH')
     os.rename(r'transpiled/cozmo_generated_program.py', r'transpiled/cozmo_generated_program'+str(timestamp)+r'.py')
     return big_string
 #reading json file
@@ -102,30 +99,26 @@ data = {
 instruc = []
 @app.route('/Lex', methods=['POST'])
 def lex():
+    global big_string
     req_data = request.get_json()
     instrucciones = req_data['lmr']
     leer_instrucciones(instrucciones)
+    big_string = generate_code(['POP_A_WHEELIE'], True)
     return '{}'.format(instruc)
 @app.route('/')
 def hello_world():
-    big_string = generate_code(['POP_A_WHEELIE'], True)
-    print(big_string)
+   
+    global big_string
     
     return render_template("index.html", big_string=big_string)
 
 def leer_instrucciones(lista):
     for i in lista:
         instruc.append(i.split(' '))
-    for x in instruc:
-        try:
-            print(x[0],x[1])
-        except IndexError:
-            print(x[0])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
     test = ['SAY']
-    #boolean = False
+    boolean = False
    # if test[0] in test1:
     #    boolean = True
-
